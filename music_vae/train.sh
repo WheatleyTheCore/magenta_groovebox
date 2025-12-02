2aa89bdb4c75918c1498bca10cbc26be9fc51006
WheatleyTheCore <alex.colwell@icloud.com>
2025-12-01 22:02:20 -0500

    updated training script to measure execution time

--- music_vae/train.sh
+++ music_vae/train.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
+start_seconds = $SECONDS
+
 music_vae_train \
 --config=groovae_2bar_groovebox \
 --run_dir=/tmp/groovebox/ \
 --mode=train \
 --tfds_name=groove/2bar-midionly
+
+end_seconds = $SECONDS
+
+duration=$((end_seconds - start_seconds))
+echo "Execution time: $duration seconds"
